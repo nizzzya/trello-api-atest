8c9aa19814b5c5b91a8fa49f05b46f66f4700b0c
NizZya <test@email.com>
2024-03-01 18:58:39 +0200

    Create CRUD v1.1
    Signed-off-by: NizZya <test@email.com>

--- src/test/java/com/swager/CardTest.java
+++ src/test/java/com/swager/CardTest.java
@@ -1,8 +1,10 @@
 package com.swager;
 
+import com.jira.api.controller.CardController;
 import com.jira.api.mode.CardDto;
 import com.jira.api.resources.ConstantsData;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,57 +19,17 @@ public class CardTest {
         var dto = new CardDto();
         dto.setIdList("65dc3c9b985276182bbb1b04");
         dto.setName("testNameNewCard");
-        baseApiClient()
-                .body(dto)
-                .post("/cards")
-                .then().log().all()
-                .assertThat()
-                .statusCode(200);
+//        baseApiClient()
+//                .body(dto)
+//                .post("/cards")
+//                .then().log().all()
+//                .assertThat()
+//                .statusCode(200);
+        Response dtoCard = new CardController()
+                .createNewCard(dto);
     }
 
-    @Test
-    @DisplayName("Get card by ID")
-    void getCard() {
-        baseApiClient()
-                .body(new CardDto())
-                .get("/cards/{id}","65e0e43461b6e61f02b978ac")
-                .then()
-                .assertThat()
-                .statusCode(200);
 
-    }
 
-    @Test
-    @DisplayName("Update card by ID")
-    void  updateCard() {
-        var dto = new CardDto();
-        dto.setName("updateNameOfCard");
-        baseApiClient()
-                .body(dto)
-                .put("/cards/{id}","65e0e43461b6e61f02b978ac")
-                .then()
-                .assertThat()
-                .statusCode(200);
-
-    }
-
-    @Test
-    @DisplayName("Delete card")
-    void deleteCard() {
-        baseApiClient()
-                .body(new CardDto())
-                .delete("/cards/{id}", "65e0e43461b6e61f02b978ac")
-                .then()
-                .assertThat()
-                .statusCode(200);
-
-    }
-
-    private RequestSpecification baseApiClient() {
-        return given()
-                .baseUri(ConstantsData.baseUrl)
-                .basePath("/1")
-                .contentType(ContentType.JSON);
-    }
 
 }
