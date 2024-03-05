6ae37e02e914f2ffc0a6de333360ff9801da97cb
NizZya <test@email.com>
2024-03-05 23:26:05 +0200

    Create CRUD v1.3 Added method Update and Delete by ID
    Signed-off-by: NizZya <test@email.com>

--- src/main/java/com/jira/api/resources/DataConstants.java
+++ src/main/java/com/jira/api/resources/DataConstants.java
@@ -4,6 +4,7 @@ public class DataConstants {
     public static final String
             base_uri = "https://api.trello.com/1",
             idListForTestWithCards = "65dc3c9b985276182bbb1b04",
+            ifCardForTestGetUpdate = "65e77c2e055f7c8cd688b4d0",
             baseCardApi = "/cards",
             get_update_delete_uri = "/cards/{id}";
 
--- src/test/java/com/swager/CardTest.java
+++ src/test/java/com/swager/CardTest.java
@@ -31,6 +31,44 @@ public class CardTest {
     @Test
     @DisplayName("Get card by ID")
     void getCardById() {
+
+        Response actualCard = new CardController()
+                .getCardById(DataConstants.ifCardForTestGetUpdate);
+
+        CardResponse cardData = actualCard.as(CardResponse.class);
+
+        Assertions.assertEquals(200, actualCard.statusCode());
+        Assertions.assertEquals(cardData.getId(), DataConstants.ifCardForTestGetUpdate);
+        Assertions.assertEquals(cardData.getIdList(), DataConstants.idListForTestWithCards);
+
+    }
+
+    @Test
+    @DisplayName("Update card")
+    void updateCardByID() {
+        var targetName = faker.name().name();
+        var dto = new CardDto(targetName);
+//        Get old data regarding card
+        Response oldCard = new CardController().getCardById(DataConstants.ifCardForTestGetUpdate);
+        CardResponse oldCardData = oldCard.as(CardResponse.class);
+        var oldName = oldCardData.getName();
+//        Update card
+        Response newCard = new CardController()
+                .updateCardById(dto, DataConstants.ifCardForTestGetUpdate);
+//        Get new data of card
+        Response actualCard = new CardController().getCardById(DataConstants.ifCardForTestGetUpdate);
+        CardResponse cardDataUpdate = actualCard.as(CardResponse.class);
+        var actualName = cardDataUpdate.getName();
+
+        Assertions.assertEquals(targetName, actualName);
+        Assertions.assertNotEquals(oldName, actualName);
+        Assertions.assertEquals(200, newCard.statusCode());
+
+    }
+
+    @Test
+    @DisplayName("Delete card")
+    void deleteCardByID() {
 //        Setup for create a new card
         var dto = new CardDto(DataConstants.idListForTestWithCards, faker.name().name());
         Response createCard = new CardController()
@@ -38,16 +76,14 @@ public class CardTest {
 //        Geg Response and actual ID of Card
         CardResponse createdCard = createCard.as(CardResponse.class);
         var cardId = createdCard.getId();
-        var targetName = createdCard.getName();
 
-        Response getCardById = new CardController()
-                .getCardById(cardId);
-        CardResponse actualCard = getCardById.as(CardResponse.class);
+        Response delete = new CardController()
+                .deleteCardById(cardId);
 
-        Assertions.assertEquals(createdCard.getName(), actualCard.getName());
-        Assertions.assertEquals(200, getCardById.statusCode());
-
-
+        delete
+                .then()
+                .assertThat()
+                .statusCode(200);
 
     }
 
